45d869ba52d5a8518ff6c7dd91acb62dd39efa6c
Juan Angosto <juan.angosto@unidadeditorial.es>
2017-04-17 12:26:45 +0200

    Add amp content innering

--- js/hydratator.js
+++ js/hydratator.js
@@ -15,18 +15,33 @@ var removeQueryString = function (url) {
     return response; 
 }
 
-var tranformContent = function () {
-    var currentUrl = window.location.href;
-    var jsonUrl = getJsonUrl(currentUrl);
-    var jsonData = getContent(jsonUrl);
-    var arrayData = JSON.parse(jsonData);
+var updatePage = function (data) {
+    var container = document.getElementById("content");
+    data = data.replace('[[[---URL---]]]', currentUrl);
+    container.innerHTML = data;
+}
 
-    document.querySelector('[data-ue-u="title"]').innerHTML = "Noticias de " + arrayData.titulo + " | EL MUNDO";
-    if (arrayData.type != "portada") {
-        document.querySelector('[data-ue-u="only-portada"]').remove();
+var tranformContent = function () {
+    var currentUrl = removeQueryString(window.location.href);
+    var urlArray = currentUrl.split("/");
+    var fileName = urlArray[urlArray.length-1];
+
+    var ampUrl = "https://jangosto.github.io/api/contents/"+fileName;
+
+    //var jsonUrl = getJsonUrl(currentUrl);
+    //var jsonData = getContent(jsonUrl);
+    //var arrayData = JSON.parse(jsonData);
+
+    //document.querySelector('[data-ue-u="title"]').innerHTML = "Noticias de " + arrayData.titulo + " | EL MUNDO";
+    //if (arrayData.type != "portada") {
+        //document.querySelector('[data-ue-u="only-portada"]').remove();
+    //}
+    //document.querySelector([data-ue-u="title"]).innerHTML = "Noticias de " + arrayData.titulo + " | EL MUNDO";
+    //document.querySelector([data-ue-u="title"]).innerHTML = "Noticias de " + arrayData.titulo + " | EL MUNDO";
+    
+    if (ampUrl.length > 0) {
+        updatePage(getContent(ampUrl));
     }
-    //document.querySelector([data-ue-u="title"]).innerHTML = "Noticias de " + arrayData.titulo + " | EL MUNDO";
-    //document.querySelector([data-ue-u="title"]).innerHTML = "Noticias de " + arrayData.titulo + " | EL MUNDO";
 }
 
 tranformContent();
