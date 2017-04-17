e773124c053c4c27b88f154844bcfab078564be9
ricardo <ricardo@el-mundo.net>
2017-04-17 12:19:26 +0200

    scope con punto

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,7 +1,7 @@
 "use strict";
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register("/sw.js", {scope: "./"}).then(function (reg) {
+	navigator.serviceWorker.register("/sw.js", {scope: "/"}).then(function (reg) {
 		console.log("serviceWorker successfully registered");
 	}).catch(function (err) {
 		console.warm("Error registering serviceWorker");
