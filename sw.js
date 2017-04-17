5487e77474feff3caba13e4319ea9bb1b246e4df
Juan Angosto <juan.angosto@unidadeditorial.es>
2017-04-17 09:28:51 +0200

    Add s_code file

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 var assets_cache_name = "v1_static";
+var content_cache_name = 'v1_dynamic';
 
-var siteDomain = "hhtps://jangosto.github.io";
+var siteDomain = "https://jangosto.github.io";
 
 var static_assets = Array(
     "https://jangosto.github.io/shell.html",
@@ -26,5 +27,39 @@ console.log("Caching static files");
 self.addEventListener('activate', function(event) {});
 
 self.addEventListener('fetch', function(event) {
-    var autocoverPattern = "";
+    var autocoverPattern =  new RegExp("^"+siteDomain+"\/([a-z0-9\-]+\/)?([a-z0-9\-]+\/)?([a-z0-9\-]+\/)?$", "i");
+
+    if (autocoverPattern.test(event.request.url)) {
+        shellRequest = new Request(siteDomain+"/shell.html");
+        event.respondWith(
+            caches.match(shellRequest).then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return caches.open(static_assets).then(function(cache) {
+                    cache.add(shellRequest).then(function() {
+                        return fetch(shellRequest);
+                    });
+                });
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                if (response) {
+                    return response;
+                }
+
+                if (event.request.method = "GET") {
+                    return caches.open(content_cache_name).then(function(cache) {
+                        return cache.add(event.request).then(function() {
+                            return fetch(event.request);
+                        });
+                    });
+                } else {
+                    return fetch(event.request);
+                }
+            })
+        );
+    }
 });
