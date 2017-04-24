d1403865e978b2d5fe27522aed07afa178873872
Juan Angosto <juan.angosto@unidadeditorial.es>
2017-04-24 11:52:07 +0200

    Fix css in autocoves

--- api/ampImporter.php
+++ api/ampImporter.php
@@ -14,6 +14,7 @@
         $entireCoverHtml = str_replace("http://e00-marca.uecdn.es/", "/", $entireCoverHtml);
         $entireCoverHtml = str_replace("http://e00-elmundo.uecdn.es/", "/", $entireCoverHtml);
         $entireCoverHtml = str_replace("http://estaticos.elmundo.es/", "/", $entireCoverHtml);
+        $entireContent = str_replace("assets/v7/css/", "/css/", $entireCoverHtml);
 
         $matches = array();
         preg_match('/<main[^>]+>(.*)<\/main>/s', $entireCoverHtml, $matches);
