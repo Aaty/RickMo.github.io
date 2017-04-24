1ef4a952ac91c52b97b6a97bd633eab50dad347b
Juan Angosto <juan.angosto@unidadeditorial.es>
2017-04-24 10:10:04 +0200

    Add new generation

--- api/ampImporter.php
+++ api/ampImporter.php
@@ -2,15 +2,18 @@
     ini_set("allow_url_fopen", true);
 
     $siteRootUrl = "https://jangosto.github.io/";
+    $siteRootPath = "/home/jangosto/projects/elmundo/pwa/";
     $originDomain = "http://www.elmundo.es/";
 
     $sections = array("index", "economia", "espana", "deportes");
 
     foreach ($sections as $section) {
-        $autocoverHtml = getFile($originDomain.$section.".html");
+        $coverHtml = getFile($originDomain.$section.".html");
+        $coverHtml = str_replace($originDomain, "/", $coverHtml);
+        $coverHtml = str_replace("http://e00-marca.uecdn.es/", "/", $coverHtml);
+        $coverHtml = str_replace("http://estaticos.elmundo.es/", "/", $coverHtml);
 
         $autocoverJson = file_get_contents("http://www.elmundo.es/json/".$section.".json");
-        
 
         $autocoverArray = json_decode($autocoverJson);
 
@@ -21,16 +24,16 @@
                 $contentData = getFile(str_replace(".html", ".json", $contentUrl));
                 $dataArray = json_decode($contentData);
 
-                $coverHtml .= '<article class="newsItem">';
                 $htmlContent = generateHtmlContent($dataArray);
                 
-                $coverHtml .= '<button class="read-later" id="read-later-'.$dataArray->id.'">Leer Más Tarde</button></article>';
-
                 file_put_contents("./contents/html/".$dataArray->id.".html", $htmlContent);
             }
         }
 
-        $coverHtml .= '</div>';
+        if (!file_exists($siteRootPath.str_replace($originDomain, "", dirname($data->url)))) {
+            mkdir($siteRootPath.str_replace($originDomain, "", dirname($data->url)), 0755, true));
+        }
+        file_put_contents("./contents/html/".$section.".html", $coverHtml);
     }
 
 
@@ -54,21 +57,16 @@ function urlExists ($url)
 
 function generateHtmlContent($data)
 {
-    global $siteRootUrl, $coverHtml;
+    global $siteRootUrl;
 
     $entireContent = getFile($data->url);
+    $entireContent = str_replace($originDomain, "/", $entireContent);
+    $entireContent = str_replace("http://e00-marca.uecdn.es/", "/", $entireContent);
+    $entireContent = str_replace("http://estaticos.elmundo.es/", "/", $entireContent);
     $matches = array();
     preg_match('/<main[^>]+>(.*)<\/main>/s', $entireContent, $matches);
     $content = $matches[0];
 
-    
-
-    if (isset($data->cintillo)) {
-        $coverHtml .= '<h2 class="kicker">'.$data->cintillo.'</h2>';
-    }
-    if (isset($data->titulo)) {
-        $coverHtml .= '<h1 class="headline"><a class="new-url" href="'.str_replace('http://www.elmundo.es/', $siteRootUrl, $data->url).'">'.$data->titulo.'</a></h1>';
-    }
     if (isset($data->multimedia) && count($data->multimedia) > 0) {
         foreach ($data->multimedia as $multimediaItem) {
             if (isset($multimediaItem->position) && $multimediaItem->position != '') {
@@ -76,12 +74,11 @@ function generateHtmlContent($data)
                     $imageExtension = pathinfo($multimediaItem->url)['extension'];
                     $imageFile = getFile(str_replace("http://e00-marca.uecdn.es/", "http://estaticos.elmundo.es/",$multimediaItem->url));
                     file_put_contents('./contents/html/images/'.$multimediaItem->id.'.'.$imageExtension, $imageFile);
+                    if (!file_exists($siteRootPath.str_replace($originDomain, "", dirname($data->url)))) {
+                        mkdir($siteRootPath.str_replace($originDomain, "", dirname($data->url)), 0755, true));
+                    }
+                    file_put_contents($siteRootPath.str_replace($originDomain, "", $data->url), $entireContent);
                     $content = str_replace($multimediaItem->url, $siteRootUrl."api/contents/html/images/".$multimediaItem->id.'.'.$imageExtension, $content);
-
-                    $coverImageWidth = "200";
-                    $coverHtml .= '<figure class="multimedia-item main" itemprop="image">';
-                    $coverHtml .= '<amp-img class="image" layout="responsive" src="'.$siteRootUrl.'api/contents/images/'.$multimediaItem->id.'.'.$imageExtension.'" height="'.$coverImageWidth*$multimediaItem->height/$multimediaItem->width.'" width="'.$coverImageWidth.'">';
-                    $coverHtml .= '</figure>';
                 }
             }
         }
