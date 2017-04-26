3ffe456bc7f87fd8127dd4e6d3ecdc8c7a746518
Juan Angosto <juan.angosto@unidadeditorial.es>
2017-04-26 14:29:32 +0200

    test with web workers from service worker

--- js/sw_register.js
+++ js/sw_register.js
@@ -4,6 +4,6 @@ if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register("/sw.js", {scope: "/"}).then(function (reg) {
 		console.log("serviceWorker successfully registered");
 	}).catch(function (err) {
-		console.warm("Error registering serviceWorker");
+		console.warn("Error registering serviceWorker");
 	});
 }
