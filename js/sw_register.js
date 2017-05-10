e8499201160b17a06e8bc17a3d43e388fc1a78f6
Juan Angosto <juan.angosto@unidadeditorial.es>
2017-05-10 09:41:29 +0200

    remove console log

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,7 +1,6 @@
 "use strict";
 
 if ('serviceWorker' in navigator) {
-    console.log(navigator);
 	navigator.serviceWorker.register("/sw.js", {scope: "/"}).then(function (reg) {
 		console.log("serviceWorker successfully registered");
 	}).catch(function (err) {
